9232784dff52493f9a1cbdc15665f96f79c07c4c
zouqingchuan <964791557@qq.com>
2020-05-06 17:29:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# IoT-HR-VUE
+
+IoT-HR 物联网工程系人力资源管理系统前端源码
+
+###服务器地址
+
+http://140.143.147.91/login?redirect=%2Fdashboard
+
+
+
+#### 前端模板
+
+初始模板基于： [https://github.com/PanJiaChen/vue-element-admin](https://github.com/PanJiaChen/vue-element-admin)
+
+模板文档： [https://panjiachen.github.io/vue-element-admin-site/zh/guide/](https://panjiachen.github.io/vue-element-admin-site/zh/guide/)
+
+#### Build Setup
+``` bash
+# 安装依赖
+npm install
+
+# 启动服务 localhost:8013
+npm run dev
+
+# 构建生产环境
+npm run build
+```
---
+++ src/settings.js
@@ -0,0 +1,50 @@
+module.exports = {
+  /**
+   * @description 网站标题
+   */
+  title: '物联网工程人力资源系统',
+  /**
+   * @description 是否显示 tagsView
+   */
+  tagsView: true,
+  /**
+   * @description 固定头部
+   */
+  fixedHeader: true,
+  /**
+   * @description 记住密码状态下的token在Cookie中存储的天数，默认1天
+   */
+  tokenCookieExpires: 1,
+  /**
+   * @description 记住密码状态下的密码在Cookie中存储的天数，默认1天s
+   */
+  passCookieExpires: 1,
+  /**
+   * @description 是否只保持一个子菜单的展开
+   */
+  uniqueOpened: true,
+  /**
+   * @description token key
+   */
+  TokenKey: '物联网工程人力资源系统',
+  /**
+   * @description 请求超时时间，毫秒（默认2分钟）
+   */
+  timeout: 1200000,
+  /**
+   * @description 是否显示logo
+   */
+  sidebarLogo: true,
+  /**
+   * 是否显示设置的底部信息
+   */
+  showFooter: true,
+  /**
+   * 底部文字，支持html语法
+   */
+  footerTxt: '© 毕业课设 2020 Zhou Qinchuan <a href="http://www.baidu.com" target="_blank">my gethub</a>',
+  /**
+   * 备案号
+   */
+  caseNumber: '重庆理工大学 物联网工程一班 11603990101'
+}
